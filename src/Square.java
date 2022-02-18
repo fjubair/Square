31f8ccde8abdae48f56bbe40e41d9900666d1bee
fjubair <50889756+fjubair@users.noreply.github.com>
2022-02-18 22:59:39 +0200

    Update Square.java

--- src/Square.java
+++ src/Square.java
@@ -1,7 +1,10 @@
 public class Square {
 
+    /**
+    * Complete this function to return the square of input number r
+    */
     public static long square(long r){
-        return r*r;
+        return 0;
     }
     
 }
